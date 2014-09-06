0c5fd3f7fa948e6b0975e1e4703daca72a19dd08
nicolasazrak <azrak.nicolas@gmail.com>
2014-09-06 19:11:26 -0300

    Agrego media y FPS camera

--- MiGrupo/EjemploAlumno.cs
+++ MiGrupo/EjemploAlumno.cs
@@ -7,6 +7,10 @@ using Microsoft.DirectX.Direct3D;
 using System.Drawing;
 using Microsoft.DirectX;
 using TgcViewer.Utils.Modifiers;
+using TgcViewer.Utils.TgcGeometry;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.Input;
+using TgcViewer.Utils.Sound;
 
 namespace AlumnoEjemplos.MiGrupo
 {
@@ -15,6 +19,13 @@ namespace AlumnoEjemplos.MiGrupo
     /// </summary>
     public class EjemploAlumno : TgcExample
     {
+
+        TgcBox box;
+        TgcBox piso;
+        TgcStaticSound sonidoDisparo;
+        TgcStaticSound sonidoRecarga;
+        TgcFpsMiCamara camara;
+        
         /// <summary>
         /// Categoría a la que pertenece el ejemplo.
         /// Influye en donde se va a haber en el árbol de la derecha de la pantalla.
@@ -40,140 +51,75 @@ namespace AlumnoEjemplos.MiGrupo
             return "MiIdea - Descripcion de la idea";
         }
 
-        /// <summary>
-        /// Método que se llama una sola vez,  al principio cuando se ejecuta el ejemplo.
-        /// Escribir aquí todo el código de inicialización: cargar modelos, texturas, modifiers, uservars, etc.
-        /// Borrar todo lo que no haga falta
-        /// </summary>
+
         public override void init()
         {
-            //GuiController.Instance: acceso principal a todas las herramientas del Framework
 
-            //Device de DirectX para crear primitivas
             Device d3dDevice = GuiController.Instance.D3dDevice;
 
-            //Carpeta de archivos Media del alumno
-            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
+            //Crear caja vacia
+            box = new TgcBox();
+            box.setPositionSize(new Vector3(0, 5, 0), new Vector3(10, 10, 10));
+            box.updateValues();
+            box.setTexture(TgcTexture.createTexture(d3dDevice, GuiController.Instance.ExamplesMediaDir + "\\Texturas\\madera.jpg"));
+
+            piso = new TgcBox();
+            piso.setPositionSize(new Vector3(0, 0, 0), new Vector3(100, 0, 100));
+            piso.updateValues();
+            piso.setTexture(TgcTexture.createTexture(d3dDevice, GuiController.Instance.ExamplesMediaDir + "\\Texturas\\pasto.jpg"));
 
 
-            ///////////////USER VARS//////////////////
-
-            //Crear una UserVar
-            GuiController.Instance.UserVars.addVar("variablePrueba");
-
-            //Cargar valor en UserVar
-            GuiController.Instance.UserVars.setValue("variablePrueba", 5451);
+            sonidoDisparo = new TgcStaticSound();
+            sonidoDisparo.loadSound(GuiController.Instance.AlumnoEjemplosMediaDir + "\\sonidos\\armas\\50_sniper_shot-Liam-2028603980.wav");
+            sonidoRecarga = new TgcStaticSound();
+            sonidoRecarga.loadSound(GuiController.Instance.AlumnoEjemplosMediaDir + "\\sonidos\\armas\\Pump_Shotgun 2x-SoundBible.com-278688366.wav");  
 
 
-
-            ///////////////MODIFIERS//////////////////
-
-            //Crear un modifier para un valor FLOAT
-            GuiController.Instance.Modifiers.addFloat("valorFloat", -50f, 200f, 0f);
-
-            //Crear un modifier para un ComboBox con opciones
-            string[] opciones = new string[]{"opcion1", "opcion2", "opcion3"};
-            GuiController.Instance.Modifiers.addInterval("valorIntervalo", opciones, 0);
-
-            //Crear un modifier para modificar un vértice
-            GuiController.Instance.Modifiers.addVertex3f("valorVertice", new Vector3(-100, -100, -100), new Vector3(50, 50, 50), new Vector3(0, 0, 0));
-
-
-
-            ///////////////CONFIGURAR CAMARA ROTACIONAL//////////////////
-            //Es la camara que viene por default, asi que no hace falta hacerlo siempre
-            GuiController.Instance.RotCamera.Enable = true;
-            //Configurar centro al que se mira y distancia desde la que se mira
-            GuiController.Instance.RotCamera.setCamera(new Vector3(0, 0, 0), 100);
-
-
-            /*
-            ///////////////CONFIGURAR CAMARA PRIMERA PERSONA//////////////////
-            //Camara en primera persona, tipo videojuego FPS
-            //Solo puede haber una camara habilitada a la vez. Al habilitar la camara FPS se deshabilita la camara rotacional
-            //Por default la camara FPS viene desactivada
-            GuiController.Instance.FpsCamera.Enable = true;
-            //Configurar posicion y hacia donde se mira
-            GuiController.Instance.FpsCamera.setCamera(new Vector3(0, 0, -20), new Vector3(0, 0, 0));
-            */
-
-
-
-            ///////////////LISTAS EN C#//////////////////
-            //crear
-            List<string> lista = new List<string>();
-
-            //agregar elementos
-            lista.Add("elemento1");
-            lista.Add("elemento2");
-
-            //obtener elementos
-            string elemento1 = lista[0];
-
-            //bucle foreach
-            foreach (string elemento in lista)
-            {
-                //Loggear por consola del Framework
-                GuiController.Instance.Logger.log(elemento);
-            }
-
-            //bucle for
-            for (int i = 0; i < lista.Count; i++)
-            {
-                string element = lista[i];
-            }
-
-
+            camara = new TgcFpsMiCamara();
+            camara.Enable = true;
+            camara.setCamera(new Vector3(-30, 10, 0), new Vector3(0, 10, 0));
+           
         }
 
 
-        /// <summary>
-        /// Método que se llama cada vez que hay que refrescar la pantalla.
-        /// Escribir aquí todo el código referido al renderizado.
-        /// Borrar todo lo que no haga falta
-        /// </summary>
-        /// <param name="elapsedTime">Tiempo en segundos transcurridos desde el último frame</param>
         public override void render(float elapsedTime)
         {
-            //Device de DirectX para renderizar
+
             Device d3dDevice = GuiController.Instance.D3dDevice;
 
+            //Renderizar caja
+            box.render();
+            piso.render();
 
-            //Obtener valor de UserVar (hay que castear)
-            int valor = (int)GuiController.Instance.UserVars.getValue("variablePrueba");
-
-
-            //Obtener valores de Modifiers
-            float valorFloat = (float)GuiController.Instance.Modifiers["valorFloat"];
-            string opcionElegida = (string)GuiController.Instance.Modifiers["valorIntervalo"];
-            Vector3 valorVertice = (Vector3)GuiController.Instance.Modifiers["valorVertice"];
-
-
-            ///////////////INPUT//////////////////
-            //conviene deshabilitar ambas camaras para que no haya interferencia
-
-            //Capturar Input teclado 
-            if (GuiController.Instance.D3dInput.keyPressed(Microsoft.DirectX.DirectInput.Key.F))
+            if (GuiController.Instance.D3dInput.keyDown(Microsoft.DirectX.DirectInput.Key.C))
             {
-                //Tecla F apretada
+                camara.swapMouseLock();
+            }
+            
+
+            if (GuiController.Instance.D3dInput.keyDown(Microsoft.DirectX.DirectInput.Key.R))
+            {
+                //sonidoRecarga.play();
             }
 
-            //Capturar Input Mouse
-            if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT))
+            if (GuiController.Instance.D3dInput.buttonDown(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT) == true)
             {
-                //Boton izq apretado
+                //sonidoDisparo.play();
             }
+
 
         }
 
-        /// <summary>
-        /// Método que se llama cuando termina la ejecución del ejemplo.
-        /// Hacer dispose() de todos los objetos creados.
-        /// </summary>
+
         public override void close()
         {
-
+            box.dispose();
+            piso.dispose();
+            sonidoDisparo.dispose();
+            sonidoRecarga.dispose();
         }
 
+
     }
+
 }
