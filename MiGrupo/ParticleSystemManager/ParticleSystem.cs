9075ce1a3093b50cd3592676d10392a9738ae1cc
mazzeog <mazzeo.gn@gmail.com>
2014-10-25 15:55:53 -0300

    Empezando sistema de particulas para la sangre

---
+++ MiGrupo/ParticleSystemManager/ParticleSystem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.DirectX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlumnoEjemplos.MiGrupo.ParticleSystemManager
+{
+    class ParticleSystem
+    {
+        List<Particle> particles;
+        Vector3 gravity;
+        Vector3 slime;
+
+        public ParticleSystem()
+        {
+
+        }
+
+        public void update()
+        {
+
+        }
+
+    }
+}
---
+++ MiGrupo/ParticleSystemManager/ParticleSystemManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlumnoEjemplos.MiGrupo.ParticleSystemManager
+{
+    class ParticleSystemManager
+    {
+        List<ParticleSystem> particleSystems;
+
+        public ParticleSystemManager( List<ParticleSystem> systems)
+        {
+            particleSystems = systems;
+        }
+
+
+        public void update()
+        {
+
+        }
+    }
+}
